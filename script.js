4d6b24f7984d8e83e6540397a9a264bae8173be5
Yuki Tsuchida <d@gahaku.tech>
2021-05-19 20:48:53 +0900

    input content

--- script.js
+++ script.js
@@ -7,18 +7,6 @@
 
     $('.wrap').css('display', 'none');
     $('.loader-bg ,.loader').height(h).css('display', 'flex');
-    $(".overlay-back").click(function () {
-        $(".overlay").fadeOut();　/*ふわっと消える*/
-    });
-    $(".products").on('click', function () {
-        let index = $('.products').index(this);
-        $(".overlay").eq(index).fadeIn();
-        $('.overlay-slide').flickity({
-            cellAlign: 'left',
-            contain: true,
-            wrapAround: true
-        });
-    });
     // menu open close
     $btn.on('click', function () {
         if (!$nav.hasClass(open)) {
@@ -34,10 +22,36 @@
 }
 )(jQuery);
 $(window).on('load', function () { //全ての読み込みが完了したら実行
-    $('.loading').delay(2500).fadeOut(800);
-    $('.loading-text').delay(1500).queue(function () {
-        $(this).addClass("animation");
+    const webStorage = function () {
+        const $loading = $('.loading');
+
+        if (sessionStorage.getItem('access')) {
+            $('.loading').css('display', 'none')
+            $('.wrap').css('display', 'flex');
+        } else {
+            $('.loading').delay(2500).fadeOut(800);
+            $('.loading-text').delay(1500).queue(function () {
+                $(this).addClass("animation");
+            });
+            $('.wrap').css('display', 'flex');
+            sessionStorage.setItem('access', 0);
+        }
+    }
+
+    webStorage();
+    $('.works-slide').flickity({
+        cellAlign: 'left',
+        contain: true,
+        wrapAround: true
     });
-    $('.wrap').css('display', 'flex');
+
 
 });
+function nameClick() {
+    sessionStorage.removeItem('access');
+    window.location.href = './index.html';
+}
+function nameWorksClick() {
+    sessionStorage.removeItem('access');
+    window.location.href = '../index.html';
+}
